2df390ab18a548607d5f341f2428863b4d71c94c
rtapia5 <113272957+rtapia5@users.noreply.github.com>
2022-11-07 09:55:30 -0800

    fixed menu.json so that javascript can read it as jsonObject

--- src/menu.js
+++ src/menu.js
@@ -18,25 +18,54 @@
 //         this.price = price;
 //     }
 // }
+async function getMenu() {
+    try {
+        //fetech the json menu file:
+        const menuData = await fetch('./src/menu.json');
 
-class Food {
-    constructor(name, price, type, calorie, description){
-        this.name = name;
-        this.price = price;
-        this.type = type;
-        this.calorie = calorie;
-        this.description =  description;
+        console.log(menuData);
+        const menus = await menuData.json();
+
+        console.log(menus);
     }
-}
-
-class Combo {
-    constructor(entre, side, beverage){
-        this.entre = entre;
-        this.side = side;
-        this.beverage = beverage;
+    catch(e) {
+        console.log('Error', e);
     }
 
-    function comboPrice(entre, side, beverage) {
-        return entre.price + side.price + beverage.price;
-    }
-}
+};
+getMenu();
+
+
+// const menuObj1 = JSON.parse('src/menu.json')
+// console.log(menuObj1)
+
+// const fs = require('fs');
+// fs.readFile('.src/menu.json', 'utf8', (err, menuObj) => {
+//      if(err){
+//         console.log(err);
+//         return;
+//      }
+//      console.log(JSON.parse(menuObj));
+// })
+
+// class Food {
+//     constructor(name, price, type, calorie, description){
+//         this.name = name;
+//         this.price = price;
+//         this.type = type;
+//         this.calorie = calorie;
+//         this.description =  description;
+//     }
+// }
+
+// class Combo {
+//     constructor(entre, side, beverage){
+//         this.entre = entre;
+//         this.side = side;
+//         this.beverage = beverage;
+//     }
+
+//     function comboPrice(entre, side, beverage) {
+//         return entre.price + side.price + beverage.price;
+//     }
+// }
