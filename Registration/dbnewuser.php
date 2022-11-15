6751cfe8d3bf47440bf9e9a25a9f7a242629495a
CesarB-CS <66040427+CesarB-CS@users.noreply.github.com>
2022-11-15 15:32:27 -0800

    registration page

---
+++ Registration/dbnewuser.php
@@ -0,0 +1,26 @@
+<?php
+$username = $_POST['username'];
+
+$email = $_POST['email'];
+
+$password = $_POST['password'];
+
+$con = new mysqli("localhost","root","","foodorder");
+
+if($con->connect_error) {
+	die("Failed to connect : ".$con->connection_error);
+} else {
+	$stmt = $con->prepare("insert into user(username,email,password)
+    values(?,?,?)");
+	$stmt->bind_param("sss",$username,$email,$password);
+	$stmt->execute();
+	echo "Registration Successfull";
+    $stmt->close();
+    $con->close();
+}
+?>
+<script>
+    setTimeout(function(){
+       window.location.href = '../index.html';
+    }, 5000);
+</script>
