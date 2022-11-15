9deeadeaf3db2b815b16bdbc84368fb9471d6b33
CesarB-CS <66040427+CesarB-CS@users.noreply.github.com>
2022-11-15 15:54:46 -0800

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$username = $_POST['username'];
+$email = $_POST['email'];
 
 $password = $_POST['password'];
 
@@ -12,8 +12,8 @@ $con = new mysqli("localhost","root","","foodorder");
 if($con->connect_error) {
 	die("Failed to connect : ".$con->connection_error);
 } else {
-	$stmt = $con->prepare("select * from user where username = ?");
-	$stmt->bind_param("s", $username);
+	$stmt = $con->prepare("select * from user where email = ?");
+	$stmt->bind_param("s", $email);
 	$stmt->execute();
 	$stmt_result = $stmt->get_result();
 	if($stmt_result->num_rows > 0) {
@@ -21,13 +21,13 @@ if($con->connect_error) {
 		if($data['password'] === $password){
 			echo "<h2>Login Successfully</h2>";
 			header('Location:/main.php?msg=Login_successfull');
-			$username = $data['username'];
-			$_SESSION['username'] = $username;
+			$email = $data['email'];
+			$_SESSION['email'] = $email;
 	} else {
-		echo "<h2>Invalid username or password</h2>";
+		echo "<h2>Invalid email or password</h2>";
 	}
 } else {
-    echo "<h2>Invalid username or password</h2>";    
+    echo "<h2>Invalid email or password</h2>";    
 }
 }
 ?>
