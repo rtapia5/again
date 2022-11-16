3a9015fc8a8b4eedb8e84495fe9cb61c353223d5
rtapia5 <113272957+rtapia5@users.noreply.github.com>
2022-11-15 21:03:34 -0800

    Update menu.js
    add images to objects

--- src/menu.js
+++ src/menu.js
@@ -1,85 +1,83 @@
 
 const food =      [
     {
-        name: "Enchiladas", category: "Entree", price: 5.00, calorie: 280, qty: 3, description: { side: ["rice", "salad", "beans"], flavor: "N/A"}
+        name: "Enchiladas", category: "Entree", price: 5.00, calorie: 280, qty: 3, description: { side: ["rice", "salad", "beans"], flavor: "N/A"}, imgSrc: "img/enchilada.jpg",
     },
 
     {
-        name: "Chile Verde", category: "Entree", price: 6.00, calorie: 380, qty: 1, description: { side: ["rice", "salad", "beans", "tortillas"], flavor: "N/A" }
+        name: "Chile Verde", category: "Entree", price: 6.00, calorie: 380, qty: 1, description: { side: ["rice", "salad", "beans", "tortillas"], flavor: "N/A" }, imgSrc: "img/chile verde.jpg",
     },
 
     {
-        name: "Chicken Fajitas", category: "Entree", price: 6.00, calorie: 370, qty: 1, description: { side: ["grilled onion", "red and green peppers", "salad", "rice"], flavor: "N/A" }
+        name: "Chicken Fajitas", category: "Entree", price: 6.00, calorie: 370, qty: 1, description: { side: ["grilled onion", "red and green peppers", "salad", "rice"], flavor: "N/A" }, imgSrc: "img/fajitas.jpg",
     },
 
     {
-        name: "Super Burrito", category: "Entree", price: 6.00, calorie: 370, qty: 1, description: { side: ["guacamole", "chease", "sour cream", "pico de gallo"], flavor: "N/A" }
+        name: "Super Burrito", category: "Entree", price: 6.00, calorie: 370, qty: 1, description: { side: ["guacamole", "chease", "sour cream", "pico de gallo"], flavor: "N/A" }, imgSrc: "img/burrito.jpg",
     },
 
     {
-        name: "Tacos", category: "Entree", price: 6.00, calorie: 370, qty: 3, description: { side: ["onion toppings", "cilantro", "salsa", "lemon"], flavor: "N/A" }
+        name: "Tacos", category: "Entree", price: 6.00, calorie: 370, qty: 3, description: { side: ["onion toppings", "cilantro", "salsa", "lemon"], flavor: "N/A" }, imgSrc: "img/taco.jpg",
     },
 
     {
-        name: "Nachos", category: "Appetizer", price: 3.00, calorie: 260, qty: "N/A", description: { side: ["melted cheese", "sour cream", "guacamole", "pico de gallo"], flavor: "N/A" }
+        name: "Nachos", category: "Appetizer", price: 3.00, calorie: 260, qty: "N/A", description: { side: ["melted cheese", "sour cream", "guacamole", "pico de gallo"], flavor: "N/A" }, imgSrc: "img/nacho.jpg",
     },
 
     {
-        name: "Guacamole Dip", category: "Appetizer", price: 3.00, calorie: 260, qty: "N/A", description: { side: ["Tortilla chips"], flavor: "N/A" }
+        name: "Guacamole Dip", category: "Appetizer", price: 3.00, calorie: 260, qty: "N/A", description: { side: ["Tortilla chips"], flavor: "N/A" }, imgSrc: "img/Guacamole.jpg",
     },
 
     {
-        name: "Taquitos", category: "Appetizer", price: 3.00, calorie: 260, qty: 3, description: { side: ["refried beans", "sour cream", "vegies", "queso fresco"], flavor: "N/A" }
+        name: "Taquitos", category: "Appetizer", price: 3.00, calorie: 260, qty: 3, description: { side: ["refried beans", "sour cream", "vegies", "queso fresco"], flavor: "N/A" }, imgSrc: "img/Taquitos.jpg",
     },
 
     {
-        name: "Tostada de ceviche", category: "Appetizer", price: 3.00, calorie: 260, qty: "N/A", description: { side: ["Choice of fish or shrimp"], flavor: "N/A" }
+        name: "Tostada de ceviche", category: "Appetizer", price: 3.00, calorie: 260, qty: "N/A", description: { side: ["Choice of fish or shrimp"], flavor: "N/A" }, imgSrc: "img/shrimp.jpg",
     },
 
     {
-        name: "Fried Mozarella", category: "Appetizer", price: 3.00, calorie: 260, qty: "N/A", description: { side: ["marinara sauce"],
-            flavor: "N/A" }
+        name: "Fried Mozarella", category: "Appetizer", price: 3.00, calorie: 260, qty: "N/A", description: { side: ["marinara sauce"], flavor: "N/A" }, imgSrc: "img/Mozarella.jpg",
     },
 
     {
-        name: "Churros", category: "Dessert", price: 4.00, calorie: 360, qty: 3, description: { side: ["homemade"], flavor: "N/A" }
+        name: "Churros", category: "Dessert", price: 4.00, calorie: 360, qty: 3, description: { side: ["homemade"], flavor: "N/A" }, imgSrc: "img/churros.jpg",
     },
 
     {
-        name: "Cheesecake", category: "Dessert", price: 4.00, calorie: 360, qty: "N/A", description: { side: ["homemade"], flavor: "N/A" }
+        name: "Cheesecake", category: "Dessert", price: 4.00, calorie: 360, qty: "N/A", description: { side: ["homemade"], flavor: "N/A" }, imgSrc: "img/cheesecake.jpg",
     },
 
     {
-        name: "Flan", category: "Dessert", price: 4.00, calorie: 360, qty: "N/A", description: { side: ["homemade"], flavor: "N/A" } 
+        name: "Flan", category: "Dessert", price: 4.00, calorie: 360, qty: "N/A", description: { side: ["homemade"], flavor: "N/A" }, imgSrc: "img/flan.jpg", 
     },
 
     {
-        name: "Shakes", category: "Dessert", price: 4.00, calorie: 360, qty: "N/A", description: { side: "N/A", flavor: ["vanilla", "chocolate"] }
+        name: "Shakes", category: "Dessert", price: 4.00, calorie: 360, qty: "N/A", description: { side: "N/A", flavor: ["vanilla", "chocolate"] }, imgSrc: "img/shakes.jpg",
     },
 
     {
-        name: "Molten Chocolate", category: "Dessert", price: 4.00, calorie: 360, qty: "N/A", description: { side: ["Topped with vanilla icecream"], flavor: "N/A"
-        }
+        name: "Molten Chocolate", category: "Dessert", price: 4.00, calorie: 360, qty: "N/A", description: { side: ["Topped with vanilla icecream"], flavor: "N/A"}, imgSrc: "img/Molten Chocolate.jpg",
     },
     
     {
-        name: "Jarritos", category: "Drinks", price: 2.00, calorie: 120, qty: 1, description: { side: "N/A", flavor: ["Fresa", "Tamarindo", "Mango", "Mandarina",  "Toronja", "Pineapple"] }
+        name: "Jarritos", category: "Drinks", price: 2.00, calorie: 120, qty: 1, description: { side: "N/A", flavor: ["Fresa", "Tamarindo", "Mango", "Mandarina",  "Toronja", "Pineapple"] }, imgSrc: "img/Jarritos.jpg",
     },
 
     {
-        name: "Soda", category: "Drinks", price: 2.00, calorie: 120, qty: 1, description: { side: "N/A", flavor: ["Various"] }
+        name: "Soda", category: "Drinks", price: 2.00, calorie: 120, qty: 1, description: { side: "N/A", flavor: ["Various"] }, imgSrc: "img/Soda.jpg",
     },
 
     {
-        name: "Aqua Fresca", category: "Drinks", price: 3.00, calorie: 120, qty: 1, description: { side: ["New daily"], flavor: "N/A" }
+        name: "Aqua Fresca", category: "Drinks", price: 3.00, calorie: 120, qty: 1, description: { side: ["New daily"], flavor: "N/A" }, imgSrc: "img/Aqua Fresca.jpg",
     },
 
     {
-        name: "Lemonade", category: "Drinks", price: 2.00, calorie: 120, qty: 1, description: { side: "N/A", flavor: ["Strawberry", "Peach"] }
+        name: "Lemonade", category: "Drinks", price: 2.00, calorie: 120, qty: 1, description: { side: "N/A", flavor: ["Strawberry", "Peach"] }, imgSrc: "img/lemonade.jpg",
     },
 
     {
-        name: "Horchata", category: "Drinks", price: 2.00, calorie: 120, qty: 1, description: { side: "N/A", flavor: [""] }
+        name: "Horchata", category: "Drinks", price: 2.00, calorie: 120, qty: 1, description: { side: "N/A", flavor: [""] }, imgSrc: "img/horchata.jpg",
     }
 ]
 
@@ -90,7 +88,58 @@ const filteredEntree = food.filter((item) => {
     return item.category == "Entree";
 });
 
-console.log(filteredEntree);
+//console.log(filteredEntree);
+
+//select entree element
+const entrees = document.querySelector(".swiper-container");
+
+function renderMenuObj(filtered) {
+    filtered.forEach((Food) => {
+        entrees.innerHTML += `
+ 
+            <div class="swiper-wrapper ${Food.category}">
+                <div class="swiper-slide">
+                <img class="card-image" src="${Food.imgSrc}" alt="${Food.name}">
+                    <div class="card-columns">
+                        <div class="cals text-left">Calories ${Food.calorie}</div>
+                        <div id="food-name" class="text-center">${Food.name}</div>
+                        <div class="price text-right">$ ${Food.price}</div>
+                    </div>
+                 </div>
+            </div>
+
+            <div class="swiper-pagination"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-scrollbar"></div>
+
+        `
+        let slider = new Swiper ('.swiper-slide', {
+            effect: 'slide',
+            
+            pagination: {
+              el: '.swiper-pagination',
+            },
+            
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+          
+            scrollbar: {
+              el: '.swiper-scrollbar',
+            },
+          });
+
+    })
+}
+
+
+
+
+
+
+
 
 const filteredAppetizer = food.filter((item) => {
     return item.category == "Appetizer";
@@ -112,15 +161,13 @@ const filteredDrink = food.filter((item) => {
 console.log(filteredDrink);
 
 
+renderMenuObj(filteredEntree);
+renderMenuObj(filteredAppetizer);
+renderMenuObj(filteredDessert);
+renderMenuObj(filteredDrink);
 
 
-// const menu = fetch('./src/menu.json')
-//         .then(res => res.json())
-//         .then(data => {
-//         //console.log(data);
-//         return data
-//     });
 
-// console.log(menu)
-
-//from this menu I need to pull out all food that has a category of "Entree", "Appetizer", "Drinks", and "Deserts"
+    // console.log(menu)
+    
+    //from this menu I need to pull out all food that has a category of "Entree", "Appetizer", "Drinks", and "Deserts"
